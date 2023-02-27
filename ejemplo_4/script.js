38f6648e2466dc8a6816ce7ce7d3bc195ed637f4
silpos <86244046+silpos@users.noreply.github.com>
2023-02-26 20:34:16 -0500

    Curriculum

--- ejemplo_4/script.js
+++ ejemplo_4/script.js
@@ -9,21 +9,28 @@ let mantequilla=4500;
 
 let huevosPastel1=12;
 let huevosPastel2=6;
+let huevosPastel3=4;
 
 
 let pastel1= harina+(huevosPastel1*huevo)+levadura+vainilla+azucar+mantequilla;
 let pastel2= harina+(huevosPastel2*huevo)+levadura+chocolate+azucar+mantequilla;
+let pastel3= harina+(huevosPastel3*huevo)+levadura+vainilla+azucar+mantequilla;
 
 console.log('el valor del pastel1 es:',pastel1, 'pesos');
 console.log('el valor del pastel2 es:',pastel2, 'pesos');
+console.log('el valor del pastel3 es:',pastel3, 'pesos');
 
-if (pastel1<pastel2) {
+if (pastel1<pastel2 && pastel1<pastel3) {
     console.log('El pastel mas economico es Paste1:',pastel1,'pesos');    
 
-} else if(pastel1>pastel2) {
+} else if(pastel2<pastel1 && pastel2<pastel3) {
     console.log('El pastel mas economico es Paste2:',pastel2,'pesos');
 
+} else if(pastel3<pastel1 && pastel3<pastel2) {
+    console.log('El pastel mas economico es Paste3:',pastel3,'pesos');
+
 } else{
-    console.log('Los 2 pasteles tinen igual precio:',pastel1, 'pesos');
+    console.log('Los 3 pasteles tienen igual precio:',pastel1, 'pesos');
 }
+
  
