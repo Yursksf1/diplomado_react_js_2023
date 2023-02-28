11ae08d4776b49e876deed04b4c3b7fb5b53f854
Ivan Orlando Diaz Ortiz <idiaz@syc.com.co>
2023-02-27 20:26:19 -0500

    taller valores de rangos de edad

---
+++ trabajoenclase/scritp.js
@@ -0,0 +1,32 @@
+edad=70
+
+lim_bebe=0
+lim_ma_bebe=3
+lim_Niños=4
+lim_ma_niño=10
+lim_adolecente=11
+lim_ma_adolecente=18
+lim_Adultos=19
+lim_ma_Adultos=60
+console.log('==> Inicio del programa')
+
+if (edad>=lim_bebe && edad<=lim_ma_bebe) {
+    console.log('bebe')
+} else if (edad>=lim_Niños && edad<=lim_ma_niño) {
+    console.log('esta niño')
+} else if (edad>=lim_adolecente && edad<=lim_ma_adolecente) {
+    console.log('adolecente')
+} 
+else if (edad>=lim_Adultos && edad<=lim_ma_Adultos) {
+    console.log('adulto')
+}
+else {
+    console.log('es un adulto mayor')
+}
+
+//  con base al codigo anterior, definir un programa para determinar el grupo que pertenece las personas segun su edad
+// Bebes : 0 - 3
+// Niños : 3 - 11
+// Adolecentes: 11 - 18
+// Adultos: 18 - 60
+// Adultos mayores: 60 o más
