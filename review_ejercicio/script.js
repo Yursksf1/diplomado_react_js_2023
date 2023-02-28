387a7604c82dcaf256ed8847efce8786d01d933b
yurley <yurs.ksf1@gmail.com>
2023-02-27 19:48:50 -0500

    check js condiciona

---
+++ review_ejercicio/script.js
@@ -0,0 +1,10 @@
+condicion = true
+console.log('==> Inicio del programa')
+
+if (condicion) {
+    console.log('esta condicion se cumple')
+} else {
+    console.log('esta condicion no se cumple')
+}
+
+console.log('==> Fin del programa')
