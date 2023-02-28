d48bab5f31eb85c8e6fb1bc6e40b261fef159d36
yurley <yurs.ksf1@gmail.com>
2023-02-27 19:59:46 -0500

    check js condiciona ejercicio 1

--- review_ejercicio/script.js
+++ review_ejercicio/script.js
@@ -1,10 +1,19 @@
-condicion = true
+LIM_ADULTO_EDAD = 18
+LIM_ADULTO_MAYOR_EDAD = 60
+
+edad = 10
+
 console.log('==> Inicio del programa')
 
-if (condicion) {
-    console.log('esta condicion se cumple')
-} else {
-    console.log('esta condicion no se cumple')
+if (edad>=LIM_ADULTO_EDAD && edad<=LIM_ADULTO_MAYOR_EDAD) {
+    console.log('esta persona es un adulto')
+} else if (edad>LIM_ADULTO_MAYOR_EDAD) {
+    console.log('esta persona es un adulto mayor')
+} else if (edad<LIM_ADULTO_EDAD) {
+    console.log('esta persona es un menor de edad')
+} 
+else {
+    console.log('esta persona es un menor de edad')
 }
 
 console.log('==> Fin del programa')
