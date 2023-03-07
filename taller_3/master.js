413fe6cadfcb6ab22e2ab08b47e08b6e9db52e0c
Yursksf1 <Yurs.Ksf1@gmail.com>
2023-03-06 19:43:12 -0500

    proposal fix minial codes

--- taller_3/master.js
+++ taller_3/master.js
@@ -25,7 +25,7 @@ console.log(``)
 //ejercicio 3, circulo radio 5
 console.log(`Inicio==> Halla perimetro y area de un circulo radio 5`)
 let Radio1 = 5;
-let pi = 3.1416;
+let pi =  Math.PI;
 let diametro1 = 2 * Radio1; 
 let perimetror1 = diametro1 * pi;
 let arear1 = pi * Math.pow(Radio1,2)
@@ -109,7 +109,7 @@ let ingredientes = ["Limon", "Aguacate", "Cebolla", "Vinagre"];
 console.log(`Mi lista: ${ingredientes}`);
 console.log(``);
 console.log(`Agrego Tomate y Aji`);
-ingredientes.push("tomate", "aji");
+ingredientes.push("Tomate", "Aji");
 console.log(`Mi nueva lista: ${ingredientes}`);
 console.log(``);
 console.log(`Ordenar Lista`);
@@ -117,13 +117,18 @@ ingredientes.sort();
 console.log(`Mi lista Ordenada: ${ingredientes}`);
 console.log(``);
 console.log(`Elimino elemento limon `);
-ingredientes.splice(2, 1)
+let aux = ingredientes.indexOf("Limon")
+ingredientes.splice(aux, 1)
 console.log(`Mi nueva lista: ${ingredientes}`);
 console.log(``);
 console.log(`Reemplazo el elemento Cebolla por Cebollita `);
-ingredientes.splice(1, 1, "Cebollita");
+aux = ingredientes.indexOf("Cebolla")
+ingredientes.splice(aux, 1, "Cebollita");
 console.log(`Mi nueva lista: ${ingredientes}`);
 console.log(``);
+ingredientes.sort();
+
+console.log(`>>> volver a ordenar Mi lista Ordenada: ${ingredientes}`);
 console.log(`Imprimir longitud de la lista `);
 let longitud = ingredientes.length;
 console.log(`La longitud es: ${longitud}`);
@@ -140,7 +145,7 @@ let gastoEscrituracion = 8000000;
 let tasaInteres = 1.5/100;
 let valorAhorrado = 50000000;
 let ahorroCesantias = 70000000;
-let numeroPagos = 20 * 12;
+let numeroPagosPactados = 20 * 12;
 let valorPrestamo = valorInmueble + gastoEscrituracion -valorAhorrado - ahorroCesantias; 
 let cuotaSinInteres = valorPrestamo / numeroPagos
 let CuotaConIntereses = 0;
@@ -148,26 +153,27 @@ let interesMensual = 0;
 let totalInteresPagado = 0;
 let valorInicial = valorPrestamo;
 
-for (numeroPagos = 1; numeroPagos <= 240; numeroPagos++) {
+for (numeroPagos = 1; numeroPagos <= numeroPagosPactados; numeroPagos++) {
     
     interesMensual = valorInicial * tasaInteres
-    console.log('Cuota sin interes', cuotaSinInteres)
-    console.log('interes Mensual', interesMensual)
+    console.log('Cuota sin interes', cuotaSinInteres.toLocaleString('es-CO', { style: 'currency', currency: 'COP' }))
+    console.log('interes Mensual', interesMensual.toLocaleString('es-CO', { style: 'currency', currency: 'COP' }))
     totalInteresPagado = totalInteresPagado + interesMensual
 
     CuotaConIntereses = cuotaSinInteres + interesMensual
-    console.log(`El valor de la cuota No ${numeroPagos} es ${CuotaConIntereses}`)
+    console.log(`El valor de la cuota No ${numeroPagos} es ${CuotaConIntereses.toLocaleString('es-CO', { style: 'currency', currency: 'COP' })}`)
 
     valorInicial = valorInicial - cuotaSinInteres
-    console.log('saldo de Deuda', valorInicial)
+    console.log('saldo de Deuda', valorInicial.toLocaleString('es-CO', { style: 'currency', currency: 'COP' }))
     console.log("")
     
 }
-console.log(`Suma total intereses pagados ${totalInteresPagado}`)
+console.log(`Suma total intereses pagados ${totalInteresPagado.toLocaleString('es-CO', { style: 'currency', currency: 'COP' })}`)
 console.log();
-console.log(`El valor del prestamo es: ${valorPrestamo}`);
+console.log(`El valor del prestamo es: ${valorPrestamo.toLocaleString('es-CO', { style: 'currency', currency: 'COP' })}`);
 console.log();
-console.log(`El valor de la cuota sin intereses es: ${cuotaSinInteres}`);
+console.log(`El valor de la cuota sin intereses es: ${cuotaSinInteres.toLocaleString('es-CO', { style: 'currency', currency: 'COP' })}`);
 console.log();
-console.log(`Suma total al final del prestamo ${totalInteresPagado + valorPrestamo}`)
+console.log(`Suma total al final del prestamo ${(totalInteresPagado + valorPrestamo).toLocaleString('es-CO', { style: 'currency', currency: 'COP' })}`)
 console.log(`Fin ==> Haciendo uso del lenguaje de programacion JavaScript. Realizar un programa para calcular el gasto total para una compra de vivienda con las siguientes condiciones`);
+
