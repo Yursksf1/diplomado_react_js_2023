16ef86459a8f8b8bb89259be7adcf401206e1956
yurley <yurs.ksf1@gmail.com>
2023-03-06 21:25:52 -0500

    js en el navegador dom, y jquery

--- browser_examples/script.js
+++ browser_examples/script.js
@@ -6,3 +6,9 @@ document.getElementsByClassName("texto")[0].textContent = "este es mi nuevo text
 // JQuery
 $("#titulo").text("Title")
 $(".texto").text("new Title")
+
+
+// Actividad:
+// - definir una lista de productos articulos del hogar ejemplo: [TV, Mesa, Ventilador ... etc ] minimo 10 articulos
+// - crear un for para visualizar dicha lista de articulos en el navegador haciendo uso de los elements tanto en Jquery con usando el DOM 
+
