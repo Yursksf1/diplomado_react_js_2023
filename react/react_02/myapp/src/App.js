e973c9a828e04b61cc151f2dc176630e56ef03cc
yurley <yurs.ksf1@gmail.com>
2023-03-22 21:12:13 -0500

    solucion ejercicio

--- react/react_02/myapp/src/App.js
+++ react/react_02/myapp/src/App.js
@@ -2,22 +2,57 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const celebraciones = [
+    {
+      "fecha": "2 - abril - 2023",
+      "titulo": "Domingo de ramos",
+      "imagen": "https://www.milenio.com/uploads/media/2022/04/08/domingo-de-ramos-significado-y.jpg",
+      "descripcion": "​También llamado Domingo de Pasión o Palmas, este día se escenifica la entrada triunfal de Jesucristo a Jerusalén, montado en un borricón, mientras la multitud le daba la bienvenida con hojas de palma en las mano"
+    },
+    {
+      "fecha": "6 - abril - 2023",
+      "titulo": "Jueves Santo",
+      "imagen": "https://img.freepik.com/vector-premium/ilustracion-vector-jueves-santo_80983-264.jpg",
+      "descripcion": "Es el día en que Jesús se reunió con sus discípulos, que eran sus amigos más cercanos, y celebró con ellos la Última Cena como gesto de despedida."
+    },
+    {
+      "fecha": "6 - abril - 2023",
+      "titulo": "Jueves Santo",
+      "imagen": "https://img.freepik.com/vector-premium/ilustracion-vector-jueves-santo_80983-264.jpg",
+      "descripcion": "Es el día en que Jesús se reunió con sus discípulos, que eran sus amigos más cercanos, y celebró con ellos la Última Cena como gesto de despedida."
+    },
+    {
+      "fecha": "6 - abril - 2023",
+      "titulo": "Jueves Santo",
+      "imagen": "https://img.freepik.com/vector-premium/ilustracion-vector-jueves-santo_80983-264.jpg",
+      "descripcion": "Es el día en que Jesús se reunió con sus discípulos, que eran sus amigos más cercanos, y celebró con ellos la Última Cena como gesto de despedida."
+    },
+    {
+      "fecha": "6 - abril - 2023",
+      "titulo": "Jueves Santo",
+      "imagen": "https://img.freepik.com/vector-premium/ilustracion-vector-jueves-santo_80983-264.jpg",
+      "descripcion": "Es el día en que Jesús se reunió con sus discípulos, que eran sus amigos más cercanos, y celebró con ellos la Última Cena como gesto de despedida."
+    },
+  ]
+  const renderCelebracion = celebraciones.map(celebracion => {
+    return (
+      <div>
+        <h1>
+          {celebracion.titulo}
+        </h1>
+        <h2>
+          {celebracion.fecha}
+        </h2>
+        <p>
+          {celebracion.descripcion}
+        </p>
+        <img src={celebracion.imagen} />
+      </div>
+    )
+  } )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {renderCelebracion }
     </div>
   );
 }
