8ca1381b83113e92cc2ca9f53d576888a8d0fe41
yurley <yurs.ksf1@gmail.com>
2023-03-31 19:08:52 -0500

    add documentation

--- react/react_10/myapp/src/App.js
+++ react/react_10/myapp/src/App.js
@@ -2,10 +2,14 @@ import React, { useRef } from 'react';
 
 function App() {
   const topRef = useRef(null);
+  const middleRef = useRef(null);
   const bottomRef = useRef(null);
 
   function handleClickButtom() {
     bottomRef.current.scrollIntoView({ behavior: 'smooth' });
+  }
+  function handleClickMiddle() {
+    middleRef.current.scrollIntoView({ behavior: 'smooth' });
   }
   function handleClickTop() {
     topRef.current.scrollIntoView({ behavior: 'smooth' });
@@ -16,7 +20,11 @@ function App() {
   return (
     <div>
       <button onClick={handleClickButtom}>Scroll abajo</button>
+      <button onClick={handleClickMiddle}>Scroll medio</button>
       <div ref={topRef}>Top</div>
+      <div style={{ height: '1000px' }}></div>
+      <div ref={middleRef}>middle</div>
+
       <div style={{ height: '1000px' }}></div>
       <div ref={bottomRef}>buttom</div>
       <button onClick={handleClickTop}>Scroll arriba</button>
